2404f100aef2d498fd8fa8a6a52d53a02a06cba3
Huy Nguyen <huy.nguyenq.tpv@one-line.com>
2023-09-20 08:43:53 +0700

    ~

--- src/app/app.controller.ts
+++ src/app/app.controller.ts
@@ -1,17 +1,27 @@
-import { Controller, Get, Inject, Req, UseGuards } from '@nestjs/common';
+import { Controller, Get, Post, Inject, Req, UseGuards } from '@nestjs/common';
 import { IAccountService, I_ACCOUNT_SERVICE } from 'src/account/interfaces';
 import { OptionalCookieGuard } from 'src/auth/guards';
 import { IRequestWithAccount } from 'src/auth/interfaces';
+import {
+  ICronService,
+  I_CRON_SERVICE,
+} from 'src/cron/interfaces/cron.service.interface';
 
 @Controller('')
 export class AppController {
   constructor(
     @Inject(I_ACCOUNT_SERVICE) private readonly accountService: IAccountService,
+    @Inject(I_CRON_SERVICE) private readonly cronService: ICronService,
   ) {}
 
   @Get('status')
   @UseGuards(OptionalCookieGuard)
   public checkHealth(@Req() { user }: IRequestWithAccount) {
     return { user: user ? this.accountService._transform(user) : null };
+  }
+
+  @Post('manual')
+  public async manual() {
+    return await this.cronService.dailyMeetingReminder();
   }
 }
--- src/cron/cron.module.ts
+++ src/cron/cron.module.ts
@@ -22,5 +22,6 @@ import { SupportModule } from 'src/public/public.module';
       useClass: CronService,
     },
   ],
+  export: [I_CRON_SERVICE],
 })
 export class CronModule {}
